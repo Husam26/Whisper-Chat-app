b325dfadb579acd7ee3293da880ab95547b7507d
Mohd Husamuddin <mohdhusamuddin2005@email.com>
2025-03-13 18:12:47 +0530

    vercel updates 2

--- backend/src/index.js
+++ backend/src/index.js
@@ -15,7 +15,7 @@
   app.use(cookieParser());
   app.use(
     cors({
-      origin: "http://localhost:5173",
+      origin: "http://localhost:5176",
       methods: ["GET", "POST", "PUT", "DELETE","PATCH"], // Allowed methods
       credentials: true, // If you need to send cookies or authorization headers
     })
